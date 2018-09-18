8f0e897ff5d966dffa3d23b5587ce5e241c12b5e
asfarley <alexander.s.farley@gmail.com>
2018-09-17 19:15:17 -0600

    Trajectory lines are rendered in red.

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ selected_movement.StateEstimates = [];
 
 function setup()
 {
-	var canvas = createCanvas(400, 1000, WEBGL);
+	var canvas = createCanvas(400, 400, WEBGL);
 	canvas.parent('display');
 }
 
@@ -13,8 +13,18 @@ function draw_movement(state_estimates_list, frame)
 		var x = state_estimate.X;
 		var y = state_estimate.Y;
 		var z = frame;
-		draw_point(x,z,y);
+		//draw_point(x,z,y);
 	});
+	
+	var i;
+	for (i = 0; i < state_estimates_list.length; i++) {
+		if(i+1<state_estimates_list.length)
+		{
+			this_state_estimate = state_estimates_list[i];
+			next_state_estimate = state_estimates_list[i+1];
+			draw_line(this_state_estimate.X,this_state_estimate.Y,frame,next_state_estimate.X,next_state_estimate.Y,frame+1);
+		}	
+	}
 }
 
 function draw_point(x,z,y)
@@ -26,11 +36,11 @@ function draw_point(x,z,y)
 	pop();
 }
 
-function draw_line(x1,y1,z1, x2,y2,z2)
+function draw_line(x1,y1,z1,x2,y2,z2)
 {
 	push();
-	stroke(100);
-	
+	stroke(255,0,0);
+	line(x1,y1,z1,x2,y2,z2);
 	pop();
 }
 
@@ -38,7 +48,11 @@ function draw()
 {
 	background(255);
 	orbitControl();
-	box();
+	
+	if(movements_list.length == 0)
+	{
+		box();
+	}
 	
 	movements_list.forEach(function(movement) {
 		draw_movement(movement.StateEstimates, movement.FirstDetectionFrame);
